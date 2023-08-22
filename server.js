9aac06617e4b95b7625b0475571bc7137ae505d3
sangjun <sangjun1389@ajou.ac.kr>
2023-08-23 02:06:20 +0900

    feat: Test with app.get

--- server.js
+++ server.js
@@ -5,15 +5,58 @@ const app = express();
 const port = 8888;
 const viewsPath = path.join(__dirname, 'views');
 
-app.set('view engine', 'ejs');
+app.engine('.html', require('ejs').renderFile);
 app.use(express.static('public'));
-app.set('views', viewsPath);
+app.set('views', viewsPath); 
+app.set('view engine', 'html');
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+let session = {};
 
 app.get('/', (req,res) => {
   res.render('index');
 })
 
+app.post('/login', (req,res)=> {
+  const {username, password} = req.body;
+  /* Login Logic part */
+  //console.log("Login successful!");
+  session = username;
+  res.redirect('/friends');
+})
+
+// End with .html
+app.get('/:pageName.html', (req, res) => {
+  const pageName = req.params.pageName;
+  res.redirect(`${pageName}`);
+});
+
+app.get('/friends', (req,res)=> {
+  username = session
+  res.render('friends', {username});
+});
+
+app.get('/setting', (req,res)=> {
+  res.render('setting');
+});
+
+app.get('/more', (req,res)=> {
+  res.render('more');
+});
+
+app.get('/chat', (req,res)=> {
+  res.render('chat');
+});
+
+app.get('/chats', (req,res)=> {
+  res.render('chats');
+});
+
+app.get('/find', (req,res)=> {
+  res.render('find');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
