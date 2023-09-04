8dad2edd3b7a490ce57bda5abf25fb8e98afe429
sangjun <sangjun1389@ajou.ac.kr>
2023-09-04 16:52:26 +0900

    Fix : Change wrong href 'setting' to 'more'

--- routes/more.js
+++ routes/more.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('setting');
+  res.render('more');
 });
 
 module.exports = router;
